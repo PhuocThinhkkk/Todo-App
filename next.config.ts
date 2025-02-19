2145efdcbdd8854d87c480e1698af8afcff8c651
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-02-19 15:11:07 +0700

    deploy gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
+  output: "export", // Xuất thành trang tĩnh
+  basePath: "/Todo-App", // Thay <REPO_NAME> bằng tên repo GitHub
+  assetPrefix: "/Todo-App/",
+  images: {
+    unoptimized: true, // Hỗ trợ ảnh trên GitHub Pages
+  },
   reactStrictMode: false, // Tắt Strict Mode
 };
 
