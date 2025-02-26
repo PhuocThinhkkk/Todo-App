d3555268436bd88974d98df4526cdca2d3d50a01
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-02-26 13:34:45 +0700

    deploy app

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   
   output: "export", // Xuất thành trang tĩnh
-  // basePath: "/Todo-App", // Thay <REPO_NAME> bằng tên repo GitHub
+  basePath: isProduction ? '/Todo-App' : '', // Thay <REPO_NAME> bằng tên repo GitHub
   // assetPrefix: "/Todo-App/",
   distDir: "dist", 
   images: {
