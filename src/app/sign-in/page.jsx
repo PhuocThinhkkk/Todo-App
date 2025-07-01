b0c86109e45664b8fc23640efc72cc3650ba35b9
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-07-01 13:06:07 +0700

    :tada::tada::tada::tada:

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -7,9 +7,9 @@ import { setDoc,  doc, getDoc } from "firebase/firestore";
 import Image from "next/image"
 import { useRouter } from "next/navigation";
 
-const cookies = new Cookies();
 export default function SignIn() { 
   const route = useRouter();
+const cookies = new Cookies();
   const hanldeSignIn = async () => { 
     const results = await signInWithPopup(auth, provider);
     const dateNow = new Date();
@@ -101,53 +101,3 @@ export default function SignIn() {
   </div> 
   );
 }
-
-export const getUserFromFireBase = async (uid) =>{
-  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
-  const collection = "userInfor";
-  const documentId = uid;
-  const firestoreUrl = `https://firestore.googleapis.com/v1/projects/${projectId}/databases/(default)/documents/${collection}/${documentId}`;
-
-  const extractField = (field) => {
-    if (field?.stringValue) return field.stringValue;
-    if (field?.integerValue) return Number(field.integerValue);
-    if (field?.arrayValue) return field.arrayValue.values.map(extractField);
-    if (field?.mapValue) return processFirestoreData(field.mapValue.fields);
-    return field; 
-  };
-
-  const processFirestoreData = (fields) => {
-    return Object.fromEntries(
-      Object.entries(fields).map(([key, value]) => [key, extractField(value)])
-    );
-  };
-
-  try{
-  const uidToken = await auth.currentUser.getIdToken();
-
-  const response = await fetch(firestoreUrl, {
-      method: "GET",
-      headers: {
-          "Authorization": `Bearer ${uidToken}`,
-      },
-  });
-  var data = await response.json();
-  if(!data.fields){
-    console.error("No data.");
-  }
-
-  console.log("🔥 Data has been taken");
-  
-  }
-  catch (error){
-    console.error("error when called API ", error);
-  } finally {
-    const userData = processFirestoreData(data.fields);
-    console.log(userData);
-    cookies.set("user-info", JSON.stringify(userData));
-    console.log("API called");
-  }
-};
- 
-
-
