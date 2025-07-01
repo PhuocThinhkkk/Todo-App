e8e92b6d63ce749a3f72851e54d21a5acde5eac8
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-07-01 20:48:28 +0700

    final release :rotating_light::rotating_light:

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,12 @@
+import { MainLayout } from "@/components/layout/main-layout"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    
+      <MainLayout>{children}</MainLayout>
+  )
+}
