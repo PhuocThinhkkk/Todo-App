b912bf7fe38a95659464dcd3c0e160f2b8659e03
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-07-01 12:39:27 +0700

    idk what i am doing

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   
-  output: "export", // Xuất thành trang tĩnh
-  basePath: isProduction ? '/Todo-App' : '', // Thay <REPO_NAME> bằng tên repo GitHub
-  // assetPrefix: "/Todo-App/",
-  distDir: "dist", 
-  images: {
-    loader: 'custom',
-    loaderFile: './ImageLoader.js'
-  },
   reactStrictMode: false, // Tắt Strict Mode
-  env: {
-    TAILWIND_DISABLE_OXIDE: 'true'
-  },
 };
 
 export default nextConfig;
